ad099a921b4fa1029a4aee851b98f063c34a9339
Sangyeob Lee <ydeja6@gmail.com>
2022-08-16 18:49:52 +0900

    [Revice]Setting

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
-
-
 const http = require("http");
 const express = require("express");
-
 const cors = require("cors");
 const morgan = require("morgan");
-
 const dotenv = require("dotenv");
 dotenv.config()
+//const routes = require("./routes");
 
 const app = express();
 const PORT = process.env.PORT;
@@ -16,19 +13,20 @@ const server = http.createServer(app);
 app.use(express.json());
 app.use(cors());
 app.use(morgan('dev'));
+//app.use(routes);
 
 //health check
 app.get("/ping", (req,res) => {
   res.json({ message: "pong"});
 });
 
-
 const start = async () => {
-  try{
-  server.listen(PORT, () => console.log(`Server is listening at ${PORT}`));
-} catch(err){
-  console.log(err);
-}
-};
-
-start();
+    try {
+      server.listen(PORT, () =>
+        console.log(`✅ Server listening on port http://localhost:${PORT} 📣`)
+      );
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  start();
