e3e700b2a5e25c21712092337cc3e94de7e9b207
yenicall <yenicall114@gmail.com>
2022-08-25 15:22:55 +0900

    [ADD]카트-CRUD-Create

--- src/services/cartService.js
+++ src/services/cartService.js
@@ -4,10 +4,6 @@ const cartDao = require("../models/cartDao");
 const createCarts = async (userId, productId, quantity) => {
   const isExists = await cartDao.existCart(userId, productId);
 
-  console.log(isExists)
-  console.log(userId)
-  console.log(productId)
-
   if (isExists) return await cartDao.plusQuantity(userId, productId);
 
   return await cartDao.createCarts(userId, productId, quantity);
